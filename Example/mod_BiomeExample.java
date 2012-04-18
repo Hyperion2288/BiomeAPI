1efe0172bad07c5b70d470872a6c2c9c2d58d687
Michael Weber <mweber1488@gmail.com>
2012-04-18 11:32:37 -0400

    Made some changes to the example

--- Example/mod_BiomeExample.java
+++ Example/mod_BiomeExample.java
@@ -1,9 +1,7 @@
 package net.minecraft.src;
 
 public class mod_BiomeExample extends BaseMod 
-{
-	@MLProp(info = "Test", name = "Test") private int testvar = 16;
-	
+{	
 	@Override
 	public void load() 
 	{
@@ -11,21 +9,33 @@ public class mod_BiomeExample extends BaseMod
 		BiomeDecorator.addDecorator("diamonds", 1, new WorldGenDiamonds());
 		
 		// Create a basic hill biome
+		if(isEnabled("superHills"))
 		(new BiomeGenSuperHill(BiomeGenBase.getNextBiomeID("superHills")))
 			.addBiomeEverything().setBiomeName("Super Hills").setMinMaxHeight(2.9f, 3.0f);
 		
 		// Create a desert replacement that edits the rate for the diamond generation
-		BiomeGenBase betterDesert = (new BiomeGenBetterDesert(BiomeGenBase.getNextBiomeID("betterDesert")))
+		BiomeGenBase betterDesert;
+		if(isEnabled("betterDesert"))
+		{
+			betterDesert = (new BiomeGenBetterDesert(BiomeGenBase.getNextBiomeID("betterDesert")))
 			.addBiomeSpawn().addBiomeVillage().addBiomeStronghold()
 			.setColor(16421912).setBiomeName("Desert").setDisableRain()
 			.setTemperatureRainfall(2.0F, 0.0F).setMinMaxHeight(0.1F, 0.2F);
-		BiomeAPI.replaceBiome(BiomeGenBase.desert, betterDesert);
+			BiomeAPI.replaceBiome(BiomeGenBase.desert, betterDesert);
+		}
 	}
 
 	@Override
 	public String getVersion() 
 	{
 		return "BiomeExample";
+	}
+	
+	private static boolean isEnabled(String key)
+	{
+		if(ModLoader.props.containsKey(key))
+			return (Integer.parseInt(ModLoader.props.getProperty(key)) != -1);
+		return true;
 	}
 
 }
